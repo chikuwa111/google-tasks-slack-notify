ff6e08658779028bc4e9c0979948f9678f56cab4
chikuwa111 <take0shun1111@gmail.com>
2020-07-11 19:49:39 +0900

    feat: Slackに送信するところまで

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,31 @@
+const main = () => {
+  const ScriptProperties = PropertiesService.getScriptProperties();
+  const TOKEN = ScriptProperties.getProperty('TOKEN');
+  const CHANNEL_ID = ScriptProperties.getProperty('CHANNEL_ID');
+
+  if (TOKEN == null || CHANNEL_ID == null) {
+    throw new Error('Failed to get properties.');
+  }
+
+  const text = "Today's Tasklist";
+  const tasks = getTasks();
+  const blocks = [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: "*Today's Tasklist*",
+      },
+    },
+    { type: 'divider' },
+    ...tasks.map(makeBlock),
+  ];
+  const isSuccess = postSlackMessage(TOKEN, CHANNEL_ID, text, blocks);
+  if (!isSuccess) {
+    throw new Error('Failed to post message.');
+  }
+};
+
 const getTasks = () => {
   const dueMax = getDueMax();
   const items = Tasks.Tasks?.list('@default', { dueMax }).items;
@@ -12,4 +40,40 @@ const getDueMax = () => {
   date.setDate(date.getDate() + 1);
   date.setUTCHours(0);
   return date.toISOString();
+};
+
+const makeBlock = ({ title, notes }: GoogleAppsScript.Tasks.Schema.Task) => ({
+  type: 'section',
+  text: {
+    type: 'mrkdwn',
+    text: `- *${title}*\n${notes ?? ''}`,
+  },
+});
+
+const postSlackMessage = (
+  token: string,
+  channelId: string,
+  text: string,
+  blocks: Array<{ type: string }>
+) => {
+  const url = 'https://slack.com/api/chat.postMessage';
+  const formData = {
+    icon_emoji: ':white_check_mark:',
+    username: 'TaskBot',
+    token,
+    channel: channelId,
+    text,
+    blocks: JSON.stringify(blocks),
+  };
+  const options = {
+    method: 'post' as const,
+    payload: formData,
+  };
+  const response = UrlFetchApp.fetch(url, options);
+  const json = JSON.parse(response.getContentText());
+  if (!json.ok) {
+    Logger.log(`Failed to post message: ${json.error}`);
+    return false;
+  }
+  return true;
 };
