97c1c35dd70c13f2a614342f765d63007cf505e2
chikuwa111 <take0shun1111@gmail.com>
2020-07-11 13:56:13 +0900

    feat: タスクを取得するところまで

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-Logger.log('Hello Google Apps Script!');
+const getTasks = () => {
+  const dueMax = getDueMax();
+  const items = Tasks.Tasks?.list('@default', { dueMax }).items;
+  return items ?? [];
+};
+
+/**
+ * 翌日の世界標準時で0時を回ったdateのRFC 3339 timestampを返す
+ */
+const getDueMax = () => {
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
+  date.setUTCHours(0);
+  return date.toISOString();
+};
